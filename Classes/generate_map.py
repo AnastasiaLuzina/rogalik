bd690d5a50eec95dee07cfc877dd83a93eca739c
Anastasia <nastayluzina777@gmail.com>
2025-04-05 20:22:07 +1000

    Добавила словари

--- Classes/generate_map.py
+++ Classes/generate_map.py
@@ -8,10 +8,8 @@ class GenerateMap:
         self.array = array
         self.point_coords = point_coords
     
-    
     def set_map_area(self):
-
-        self.array = [] #нужно всегда писать при обращеннии адрес на класс self!!!!
+        self.array = [] # Всегда пишем self для обращения к переменным класса ат то плохо
         for i in range(self.height):
             row = []
             for j in range(self.width):
@@ -23,118 +21,67 @@ class GenerateMap:
             print(' '.join(row))
 
 
-        
-    
-
-
 class GenerateRooms(GenerateMap):
 
     def __init__(self, height, width):
         super().__init__(height, width) 
 
- 
-    def check_method (self, x, y):
-            return (x < 0 or y < 0 or x >= self.height or y >= self.width )
+    def check_method(self, x, y):
+        return (x < 0 or y < 0 or x >= self.height or y >= self.width) # если выход за границу карты то эту клетку скипаем
 
-    def intersection_recognition (self, first_x, first_y, random_height_room, random_width_room):
+    def intersection_recognition(self, first_x, first_y, random_height_room, random_width_room):
+        for i in range(random_height_room):
+            for j in range(random_width_room):
 
-        sides_of_clash = []
+                if self.check_method(first_x+i, first_y+j):
+                    continue 
 
-        stop_x = 0
-        stop_y = 0
+                if self.array[first_x + i][first_y +j] != ' ': 
+                    return True
+        return False
 
-        for p in range(random_width_room):
+    def generate_wall(self):
 
-            if self.check_method(first_x, first_y + p + 2): continue
-
-            if self.array[first_x][first_y + p + 2] == "║" or self.array[first_x][first_y + p +2] == "═":
-                stop_x = first_x
-                stop_y = first_y + p
-                sides_of_clash.append("vertically")
-
-        for p in range(random_height_room):
-
-            if self.check_method(first_x  + p + 2, first_y): continue
-
-            if self.array[first_x + p + 2][first_y] == "║" or self.array[first_x  + p + 2][first_y] == "═":
-                stop_x = first_x + p
-                stop_y = first_y 
-                sides_of_clash.append("horizontal")
-
-
-        if not sides_of_clash:
-            for i in range(random_height_room):
-                for j in range(random_width_room):
-   
-                        self.array[first_x][first_y + j] = "═"
-                        self.array[first_x + random_height_room - 1][first_y + j] = "═"
-
-                        self.array[first_x +i][first_y] = "║"
-                        self.array[first_x + i][first_y + random_width_room - 1] = "║"
-
-        if "vertically" in sides_of_clash or "horizontal" in sides_of_clash:
-
-                if "vertically" in sides_of_clash and "horizontal" in sides_of_clash:
-                    for i in range(stop_x):
-                        for j in range(stop_y):
-
-                            self.array[first_x][first_y + j] = "═"
-                            self.array[stop_x + random_height_room - 1][first_y + j] = "═"
-
-                            self.array[first_x +i][first_y] = "║"
-                            self.array[first_x + i][stop_y + random_width_room - 1] = "║"
-
-                if "vertically" in sides_of_clash and not "horizontal" in sides_of_clash:
-                    for i in range(random_height_room):
-                        for j in range(stop_y):
-
-                            self.array[first_x][first_y + j] = "═"
-                            self.array[stop_x + random_height_room - 1][first_y + j] = "═"
-
-                            self.array[first_x +i][first_y] = "║"
-                            self.array[first_x + i][first_y + random_width_room - 1] = "║"
-
-                if  "horizontal" in sides_of_clash and not "vertically" in sides_of_clash:
-                    for i in range(stop_x):
-                        for j in range(random_width_room):
-
-                            self.array[first_x][first_y + j] = "═"
-                            self.array[first_x + random_height_room - 1][first_y + j] = "═"
-
-                            self.array[first_x +i][first_y] = "║"
-                            self.array[first_x + i][stop_y + random_width_room - 1] = "║"
-                
-
-
-
-
-    def generate_wall (self):
-    
-        count_of_rooms = random.randint(1, 9) 
+        count_of_rooms = random.randint(15, 20) 
 
         for n in range(count_of_rooms):
 
-            random_height_room = random.randint(5, 15)
-            random_width_room = random.randint(5, 15)
+            self.point_coords[n] = [] # создаем пустые ключи для словаря с тунелями
 
-            
+            random_height_room = random.randint(5, 20)
+            random_width_room = random.randint(5, 20)
+
             if (random_height_room > self.height or random_width_room > self.width):
                 continue
 
             first_x = random.randint(0, self.height - random_height_room)
             first_y = random.randint(0, self.width - random_width_room)
 
-            if self.array[first_x][first_y] == "." or self.array[first_x][first_y] == "║" or self.array[first_x][first_y] == "═" :
-                continue
+            if self.intersection_recognition(first_x, first_y, random_height_room, random_width_room):
+                count_of_rooms +=1
+                continue  # Если пересекается -> просто по новой запускаем цикл емае как я до этого не додумалась с первого раза епрст
 
-            self.intersection_recognition(first_x, first_y, random_height_room, random_width_room)
+            for i in range(random_height_room):
+                self.array[first_x + i][first_y] = "║"
+                self.array[first_x + i][first_y+ random_width_room - 1] = "║"
 
-            for i in range(1, random_height_room - 1):  # Внутри вертикальных стенок
-                for j in range(1, random_width_room - 1):  # Внутри горизонтальных стенок
-                    self.array[first_x + i][first_y + j] = "."
-                
-                                
-map = GenerateRooms(50, 50) #нужно инициализировать через наследника
+                self.point_coords[n].append((first_x + i, first_y))
+                self.point_coords[n].append((first_x + i, first_y+ random_width_room - 1)) # словарик для туннелей
+
+
+
+            for j in range(random_width_room):
+                self.array[first_x][first_y +j] = "═"
+                self.array[first_x + random_height_room - 1][first_y + j] = "═"
+
+                self.point_coords[n].append((first_x, first_y +j))
+                self.point_coords[n].append((first_x + random_height_room - 1, first_y + j)) # словарик координат для туннелей
+
+            for i in range(1, random_height_room - 1):
+                for j in range(1, random_width_room - 1):
+                    self.array[first_x+ i][first_y + j] = "."
+
+map = GenerateRooms(70, 70)
 map.set_map_area()
 map.generate_wall()
 map.print_map()
