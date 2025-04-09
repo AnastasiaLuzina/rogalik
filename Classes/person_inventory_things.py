6e55dd889cab511ef8421e8701fe7d0fabb9dad6
Anastasia <nastayluzina777@gmail.com>
2025-04-10 01:06:54 +1000

    Реализация генерации объектов на карте + создание общего класса Render lдля отрисовки

--- Classes/person_inventory_things.py
+++ Classes/person_inventory_things.py
@@ -4,11 +4,11 @@ class Position:
         self.y = y
 
 class Person:
-    def __init__(self, health, position =(), inventory, active_slot = None):
+    def __init__(self, health, position=Position(0, 0), inventory=None, active_slot=None):
         self.health = health
         self.max_health = health
         self.position = position        
-        self.inventory = Inventory(5)
+        self.inventory = inventory if inventory is not None else Inventory(5)
         self.active_slot = active_slot
 
 class Hero(Person):
@@ -21,70 +21,57 @@ class Enemy(Person):
         super().__init__(health, position)
         self.symbol = symbol
 
-
-
 class Inventory:
-    def __init__ (self, count_of_slots, items = {}):
+    def __init__(self, count_of_slots, items=None):
         self.count_of_slots = count_of_slots
-        self.items = items
+        self.items = items if items is not None else {}
 
 class Items:
     def __init__(self, title, type):
         self.title = title
         self.type = type
 
-class Sword:
+class Sword(Items):
     def __init__(self, title, type, damage, symbol):
         super().__init__(title, type)
         self.damage = damage
         self.symbol = symbol
 
-
-class Bomb:
+class Bomb(Items):
     def __init__(self, title, type, damage, self_damage, symbol):
         super().__init__(title, type)
         self.damage = damage
         self.self_damage = self_damage
         self.symbol = symbol
 
-
-class Dog:
+class Dog(Items):
     def __init__(self, title, type, damage, symbol):
         super().__init__(title, type)
         self.damage = damage
         self.symbol = symbol
 
-
-class Potion:
-    def __init__(self, title, type, damage_, symbol):
+class Potion(Items):
+    def __init__(self, title, type, damage, symbol):
         super().__init__(title, type)
         self.damage = damage
         self.symbol = symbol
 
-
-class Shield:
+class Shield(Items):
     def __init__(self, title, type, save_from_damage, symbol):
         super().__init__(title, type)
         self.save_from_damage = save_from_damage
         self.symbol = symbol
 
-
-class Medicine:
+class Medicine(Items):
     def __init__(self, title, type, medication, symbol):
         super().__init__(title, type)
         self.medication = medication
         self.symbol = symbol
-
-
-
 hero_position = Position(3, 5)
-
 hero = Hero(health=100, position=hero_position, symbol="@")
-
 sword = Sword("Меч", "attack", 50, "🗡")
 bomb = Bomb("Бомба", "attack", 100, 50, "☭")
-dog_friend = Bomb("Пёся", "attack", 50, "𓃠")
-position = Position("Зелье", "attack", 5, "🧪")
-
+dog_friend = Dog("Пёся", "attack", 50, "𓃠")
+potion = Potion("Зелье", "attack", 5, "🧪")
 shield = Shield("Shit", "block", 20, "⛨")
 medicine = Medicine("Хилка", "medicine", 20, "✙")
--- Classes/rogalik.py
+++ Classes/rogalik.py
@@ -2,11 +2,11 @@ import random
 
 class GenerateMap:
 
-    def __init__ (self, height , width, array= None, point_coords = {}):
+    def __init__ (self, height , width, array= None):
         self.height = height
         self.width = width
         self.array = array
-        self.point_coords = point_coords
+        
     
     def set_map_area(self):
         self.array = [] # Всегда пишем self для обращения к переменным класса ат то плохо
@@ -16,15 +16,15 @@ class GenerateMap:
                 row.append(' ')  
             self.array.append(row)
     
-    def print_map(self):
-        for row in self.array:
-            print(' '.join(row))
+    
 
 
 class GenerateRooms(GenerateMap):
 
-    def __init__(self, height, width):
-        super().__init__(height, width) 
+    def __init__(self, height, width, coords_of_walls = {}, coords_of_rooms_area = []):
+        super().__init__(height, width)
+        self.coords_of_walls = coords_of_walls 
+        self.coords_of_rooms_area = coords_of_rooms_area 
 
     def check_method(self, x, y):
         return (x < 0 or y < 0 or x >= self.height or y >= self.width) # если выход за границу карты то эту клетку скипаем
@@ -40,13 +40,24 @@ class GenerateRooms(GenerateMap):
                     return True
         return False
 
+    def available_positions(self): #работаем с map
+        self.coords_of_rooms_area = []
+        for i in range(len(self.array)):
+            for j in range(len(self.array[i])):
+                if self.array[i][j] == '.':
+                    self.coords_of_rooms_area.append((i, j))
+
+        random_index = random.choice(self.coords_of_rooms_area) 
+        
+        return  random_index
+
     def generate_wall(self):
 
-        count_of_rooms = random.randint(15, 20) 
+        count_of_wallss = random.randint(15, 20) 
 
-        for n in range(count_of_rooms):
+        for n in range(count_of_wallss):
 
-            self.point_coords[n] = [] # создаем пустые ключи для словаря с тунелями
+            self.coords_of_walls[n] = [] # создаем пустые ключи для словаря с тунелями
 
             random_height_room = random.randint(5, 20)
             random_width_room = random.randint(5, 20)
@@ -58,15 +69,15 @@ class GenerateRooms(GenerateMap):
             first_y = random.randint(0, self.width - random_width_room)
 
             if self.intersection_recognition(first_x, first_y, random_height_room, random_width_room):
-                count_of_rooms +=1
+                count_of_wallss +=1
                 continue  # Если пересекается -> просто по новой запускаем цикл емае как я до этого не додумалась с первого раза епрст
 
             for i in range(random_height_room):
                 self.array[first_x + i][first_y] = "║"
                 self.array[first_x + i][first_y+ random_width_room - 1] = "║"
 
-                self.point_coords[n].append((first_x + i, first_y))
-                self.point_coords[n].append((first_x + i, first_y+ random_width_room - 1)) # словарик для туннелей
+                self.coords_of_walls[n].append((first_x + i, first_y))
+                self.coords_of_walls[n].append((first_x + i, first_y+ random_width_room - 1)) # словарик для туннелей
 
 
 
@@ -74,14 +85,54 @@ class GenerateRooms(GenerateMap):
                 self.array[first_x][first_y +j] = "═"
                 self.array[first_x + random_height_room - 1][first_y + j] = "═"
 
-                self.point_coords[n].append((first_x, first_y +j))
-                self.point_coords[n].append((first_x + random_height_room - 1, first_y + j)) # словарик координат для туннелей
+                self.coords_of_walls[n].append((first_x, first_y +j))
+                self.coords_of_walls[n].append((first_x + random_height_room - 1, first_y + j)) # словарик координат для туннелей
 
             for i in range(1, random_height_room - 1):
                 for j in range(1, random_width_room - 1):
                     self.array[first_x+ i][first_y + j] = "."
 
+
+class Person:
+    def __init__(self, health, position):
+        self.health = health
+        self.max_health = health
+        self.position = position        
+
+class Hero(Person):
+    def __init__(self, health, position, symbol):
+        super().__init__(health, position)
+        self.symbol = symbol
+
+class Enemy(Person):
+    def __init__(self, health, position, symbol):
+        super().__init__(health, position)
+        self.symbol = symbol
+
+
+class RenderNewFrame:
+    def __init__(self, game_map):
+        self.game_map = game_map
+
+    def print_map(self):
+        for row in self.game_map.array: #  надо писать в таком случае не self.array а  self.game_map.array
+            print(' '.join(row))
+
+    def place_person(self, person):
+        x, y = person.position
+        self.game_map.array[x][y] = person.symbol
+
+
+
+
 map = GenerateRooms(70, 70)
 map.set_map_area()
 map.generate_wall()
-map.print_map()
+
+frame = RenderNewFrame(map)
+
+hero = Hero(100, map.available_positions(), "@")
+
+frame.place_person(hero)
+
+frame.print_map() 
